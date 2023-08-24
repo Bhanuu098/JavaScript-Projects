5051c5b19d57a9d88c3ce63e5f44139b731c183e
Bhanuu098 <137154090+Bhanuu098@users.noreply.github.com>
2023-08-24 09:18:35 +0100

    Add files via upload
    Basic Javascript Projects Assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,18 @@
+window.alert("Hello World");
+document.write("Hello,World!!");
+
+var A="This is a String"
+document.write(A);
+
+document.write(" Hello, \"What is Your Name?\"")
+var B=" Hello"+" there"
+document.write(B);
+
+var C=B+" My name is Bhanuka"
+document.write(C); //printing variable C
+
+var Rainbow="Colors", V=" Violet", I=" Indigo", B=" Blue", G=" Green", Y=" yellow", O=" Orange", R=" Red"
+document.write(I);
+
+var Z= 8*9+7
+document.write(Z);

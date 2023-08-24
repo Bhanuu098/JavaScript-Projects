39f73935b9bbe269e8731f08f1349438fab7a96b
Bhanuu098 <137154090+Bhanuu098@users.noreply.github.com>
2023-08-24 09:33:29 +0100

    Add files via upload
    Project2_functions assignment

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,9 @@
+function My_First_Function(){
+    var A="My name is Bhanuka Bansal";
+    document.getElementById("Button_Text").innerHTML = A;
+}
+function myFunction(){
+    var sentence="I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
